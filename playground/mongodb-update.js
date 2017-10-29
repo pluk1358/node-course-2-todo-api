3938d1437a5d81507c63d6c8753be7e952233b5d
Kawin Teeraprapkajorndet <kawin88@gmail.com>
2017-10-29 14:12:53 +0700

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,27 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('59f568b5f6a8ee03381a9536')
+  }, {
+    $set: {
+      name: 'Kevin'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  })
+
+  // db.close();
+});
