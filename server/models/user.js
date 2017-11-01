44b0ca5adfe4010598ff9b2f144be9d311595de3
Kawin Teeraprapkajorndet <kawin88@gmail.com>
2017-11-01 13:23:34 +0700

    Add GET users/me and authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -45,7 +45,8 @@ UserSchema.methods.generateAuthToken = function() {
   var user = this;
   var access = 'auth';
   var token = jwt.sign({
-    _id: user._id.toHexString(), access
+    _id: user._id.toHexString(), 
+    access
   }, 'abc123').toString();
 
   user.tokens.push({access, token});
@@ -53,6 +54,23 @@ UserSchema.methods.generateAuthToken = function() {
     return token;
   });
 };
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+}
 
 var User = mongoose.model('User', UserSchema);
 
