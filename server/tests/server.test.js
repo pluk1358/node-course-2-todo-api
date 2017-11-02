42f0280dd7963353520a1ece59897054deda5a77
Kawin Teeraprapkajorndet <kawin88@gmail.com>
2017-11-02 12:53:43 +0700

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -292,4 +292,24 @@ describe('POST /users/login', () => {
         }).catch(done);
     });
   });
-})
+});
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .send()
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch(done);
+      })
+  });
+});
